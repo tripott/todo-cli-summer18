4293bf27fd7b9a733f3223b4ae82ddbdea963c0d
Brian Liotti <bdliotti@gmail.com>
2018-06-11 14:01:40 -0400

    nothing much

--- cmd/init.js
+++ cmd/init.js
@@ -1,8 +1,7 @@
 //
 // 1) if the todos.json file exists then return "todos.json already exists and cannot be overwritten"
-// 2)
-//
-//
+// 2) else it creates a todos.json file in the root project dir.
+// 3)
 //
 
 const fs = require('fs')
